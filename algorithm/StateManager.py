09ea0945752c8c9687264ceae599d48cccaf2254
Philipp Lentzen <code@philipplentzen.dev>
2025-05-05 10:40:53 +0200

    Run pre-commit hook on all files

--- algorithm/StateManager.py
+++ algorithm/StateManager.py
@@ -1,15 +1,17 @@
 class StateManager:
     """
-    Currently only in use to keep track of which constraints are 
-    fullfilled by the algorithm. When the solutions are saved, it is 
+    Currently only in use to keep track of which constraints are
+    fullfilled by the algorithm. When the solutions are saved, it is
     important to also save the list of constraints.
     This class can of couse be expanded if any other global variables
     are needed.
     """
+
     def __init__(self):
         self.constraints = []
 
     def clear(self):
         self.constraints.clear()
+
 
 state = StateManager()
