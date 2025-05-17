bca080334dfe618e566f432b4f66924f9d27f613
Tuhina Kumar <42111875+tuhinak@users.noreply.github.com>
2025-05-17 22:23:23 +0200

    Update not_too_long_shifts.py

--- algorithm/building_constraints/not_too_long_shifts.py
+++ algorithm/building_constraints/not_too_long_shifts.py
@@ -6,7 +6,7 @@ def not_too_long_shifts(model, shifts, num_days, num_persons, num_shifts, shift_
     
     Args:
         model: cp_model instance.
-        shifts: 3D list [p][d][s] of BoolVars indicating assignments.
+        shifts: Dict[(p,d,s)] of BoolVars indicating assignments.
         num_days: Total number of days.
         num_persons: Total number of staff members.
         num_shifts: Total number of shifts per day.
@@ -17,8 +17,8 @@ def not_too_long_shifts(model, shifts, num_days, num_persons, num_shifts, shift_
         for d in range(num_days - 2):
             for s in long_shift_idxs:
                 model.AddBoolOr([
-                    shifts[p][d][s].Not(),
-                    shifts[p][d + 1][s].Not(),
-                    shifts[p][d + 2][s].Not()
+                    shifts[(p, d, s)].Not(),
+                    shifts[(p, d + 1, s)].Not(),
+                    shifts[(p, d + 2, s)].Not()
                 ])
 
