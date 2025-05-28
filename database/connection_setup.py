ffe98199c043894b6fece50008e8d4606036925f
Linda Temme <temmelin@fit.cvut.cz>
2025-05-28 10:40:29 +0200

    Restructure basic connection file

--- database/connection_setup.py
+++ database/connection_setup.py
@@ -4,35 +4,60 @@ from dotenv import load_dotenv
 import os
 import json
 
-# .env-Datei laden
+# Load the .env-file for login purposes
 load_dotenv()
 
-# Umgebungsvariablen auslesen
-server = os.getenv("DB_SERVER")
-database = os.getenv("DB_NAME")
-username = os.getenv("DB_USER")
-password = os.getenv("DB_PASSWORD")
 
-# SQL-Verbindung aufbauen
-conn_str = (
-    f"DRIVER={{ODBC Driver 18 for SQL Server}};"
-    f"SERVER={server};"
-    f"DATABASE={database};"
-    f"UID={username};"
-    f"PWD={password};"
-    "TrustServerCertificate=yes;"
-)
-conn = pyodbc.connect(conn_str)
+def get_db_connection():
+    """Create a basic connection to the TimeOffice database."""
+    server = os.getenv("DB_SERVER")
+    database = os.getenv("DB_NAME")
+    username = os.getenv("DB_USER")
+    password = os.getenv("DB_PASSWORD")
 
-# SQL-Test-Abfrage
-query = "SELECT * FROM TPersonal WHERE Prim=459"
-df = pd.read_sql(query, conn)
+    conn_str = (
+        f"DRIVER={{ODBC Driver 18 for SQL Server}};"
+        f"SERVER={server};"
+        f"DATABASE={database};"
+        f"UID={username};"
+        f"PWD={password};"
+        "TrustServerCertificate=yes;"
+    )
+    return pyodbc.connect(conn_str)
 
-# In JSON konvertieren
-json_output = df.to_json(orient="records", force_ascii=False)
 
-# In Datei schreiben
-with open("daten_export.json", "w", encoding="utf-8") as f:
-    f.write(json_output)
+def get_correct_path(filename):
+    """Return the correct path to store the given file in."""
+    # Get the defined folder names out of the .env-file
+    base_folder = os.getenv("BASE_OUTPUT_FOLDER")
+    sub_folder = os.getenv("SUB_OUTPUT_FOLDER")
+    current_dir = os.path.dirname(os.path.abspath(__file__))
 
-print("✅ Export abgeschlossen – daten_export.json erstellt")
+    # Create the output path to store the file in
+    target_dir = os.path.join(current_dir, "..", base_folder, sub_folder)
+    target_dir = os.path.abspath(target_dir)
+    output_path = os.path.join(target_dir, filename)
+    return output_path
+
+
+def export_personal_data_to_json(conn, filename="employees.json"):
+    """Export all personal staff information found within TPersonal and create a JSON-file.
+    CURRENTLY ONLY TEST PURPOSE ON THIS BRANCH"""
+    query = "SELECT * FROM TPersonal WHERE Prim=459"
+    df = pd.read_sql(query, conn)
+    json_output = df.to_json(orient="records", force_ascii=False)
+    store_path = get_correct_path(filename)
+    # Create or overwrite file in target directory
+    with open(store_path, "w", encoding="utf-8") as f:
+        f.write(json_output)
+    print(f"✅ Export abgeschlossen – {filename} erstellt")
+
+
+def main():
+    conn = get_db_connection()
+
+    export_personal_data_to_json(conn)
+
+
+if __name__ == "__main__":
+    main()
