7940e501861c3c447a35b881c0bb13943e8b2db6
paulgilles <paul.gilles@gmx.net>
2025-05-31 16:06:14 +0200

    including "actual" (IST) and "target" (SOLL) in target working minutes

--- algorithm/building_constraints/target_working_minutes.py
+++ algorithm/building_constraints/target_working_minutes.py
@@ -60,12 +60,8 @@ def create_total_work_time_variables(
 def add_target_working_minutes(
     model,
     employees,
-    shifts,
     total_work_times,
-    num_days,
-    num_shifts,
     target_min_data,
-    vacation_data=None,
 ):
     """
     Adds a constraint to the model that ensures each employee's total working time
@@ -83,7 +79,6 @@ def add_target_working_minutes(
     """
 
     employees_target_minutes = target_min_data["employees"]
-    shift_durations = target_min_data["shift_durations"]
     tolerance_less = target_min_data["tolerance_less"]
     tolerance_more = target_min_data["tolerance_more"]
 
@@ -91,6 +86,7 @@ def add_target_working_minutes(
 
     employee_names_to_target = {
         employees_target_minutes[i]["name"]: employees_target_minutes[i]["target"]
+        - employees_target_minutes[i]["actual"]  # SOLL - IST
         for i in range(len(employees_target_minutes))
     }
 
@@ -103,31 +99,6 @@ def add_target_working_minutes(
             continue  # skip if no target time provided
         else:
             target_minutes = employee_names_to_target[employee["name"]]
-
-            # handle vacation days, need to be substracted from target minutes
-            if StateManager.state.switch["free_shifts"]:
-                if vacation_data is not None:
-                    correct_employee = None
-                    for employee_candidate in vacation_data["employees"]:
-                        if employee_candidate["name"] == employee["name"]:
-                            correct_employee = employee_candidate
-                    if correct_employee is not None:
-                        # employee has vacation days
-                        num_free_days = len(correct_employee["free_days"])
-                        target_minutes -= num_free_days * min(shift_durations.values())
-                        print(
-                            employee["name"],
-                            n_idx,
-                            correct_employee["name"],
-                            target_minutes,
-                            num_free_days,
-                        )
-                else:
-                    raise TypeError(
-                        "You need to provide `vacation_data` to"
-                        " target working minutes if constraint"
-                        " free shifts and vacation days is active."
-                    )
 
             total_work_time = total_work_times[employee["name"]]
 
