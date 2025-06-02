a11db085d0e3bf5bbbbe96d438e3e137e47bf4e2
paulgilles <paul.gilles@gmx.net>
2025-06-02 08:47:24 +0200

    changed Keys in (FreeShifts...).py to fit new dataformat (forbidden...,vacation...,...)

--- algorithm/building_constraints/free_shifts_and_vacation_days.py
+++ algorithm/building_constraints/free_shifts_and_vacation_days.py
@@ -20,9 +20,9 @@ def add_free_shifts_and_vacation_days(
     if "employees" in constraints:
         for employee in constraints["employees"]:
             employee_idx = name_to_index[employee["name"]]
-            if "free_days" in employee:
+            if "vacation_days" in employee:
                 for day_int in employee[
-                    "free_days"
+                    "vacation_days"
                 ]:  # day_int corresponds to real date, starts at 1
                     for s in range(num_shifts):
                         model.Add(
@@ -32,10 +32,50 @@ def add_free_shifts_and_vacation_days(
                         model.Add(
                             shifts[(employee_idx, day_int - 2, 2)] == 0
                         )  # no night shift before vacation
-            if "free_shifts" in employee and len(employee["free_shifts"][0]) > 0:
-                for day_int, shift_name in employee["free_shifts"]:
+
+            # forbidden = not vacation, but regular days which the person does not work
+            if "forbidden_days" in employee:
+                for day_int in employee["forbidden_days"]:
+                    for s in range(num_shifts):
+                        model.Add(
+                            shifts[(employee_idx, day_int - 1, s)] == 0
+                        )  # day_int to day_idx
+
+            if (
+                "vacation_shifts" in employee
+                and len(employee["vacation_shifts"][0]) > 0
+            ):
+                for day_int, shift_name in employee["vacation_shifts"]:
                     shift_idx = shift_names_to_index[shift_name]
                     model.Add(shifts[(employee_idx, day_int - 1, shift_idx)] == 0)
+
+            # currently handled the same as vacation shifts
+            if (
+                "forbidden_shifts" in employee
+                and len(employee["forbidden_shifts"][0]) > 0
+            ):
+                for day_int, shift_name in employee["forbidden_shifts"]:
+                    shift_idx = shift_names_to_index[shift_name]
+                    model.Add(shifts[(employee_idx, day_int - 1, shift_idx)] == 0)
+
+            # whishes
+            if "wish_days" in employee:
+                for day_int in employee[
+                    "wish_days"
+                ]:  # day_int corresponds to real date, starts at 1
+                    for s in range(num_shifts):
+                        model.Add(
+                            shifts[(employee_idx, day_int - 1, s)] == 0
+                        )  # day_int to day_idx
+                    if day_int >= 2:  # prevent index of -1
+                        model.Add(
+                            shifts[(employee_idx, day_int - 2, 2)] == 0
+                        )  # no night shift before vacation
+            if "wish_shifts" in employee and len(employee["wish_shifts"][0]) > 0:
+                for day_int, shift_name in employee["wish_shifts"]:
+                    shift_idx = shift_names_to_index[shift_name]
+                    model.Add(shifts[(employee_idx, day_int - 1, shift_idx)] == 0)
+
     else:
         raise ValueError(
             "Dataformat `free shifts` does not fit. Key `employees` is missing."
