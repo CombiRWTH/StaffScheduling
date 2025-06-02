45f297916c831c25a55c1aeb361fad773a6f0cfa
paulgilles <paul.gilles@gmx.net>
2025-06-02 08:53:58 +0200

    added option to build in wishes as hard constraint

--- algorithm/building_constraints/free_shifts_and_vacation_days.py
+++ algorithm/building_constraints/free_shifts_and_vacation_days.py
@@ -14,6 +14,8 @@ def add_free_shifts_and_vacation_days(
     """
     Adds constraints for requested days off and shift exclusions.
     """
+    wishes_as_hard_constraint = StateManager.state.switch["wishes_as_hard_constraint"]
+
     name_to_index = {employee["name"]: idx for idx, employee in enumerate(employees)}
     shift_names_to_index = {"F": 0, "S": 1, "N": 2}
 
@@ -59,22 +61,23 @@ def add_free_shifts_and_vacation_days(
                     model.Add(shifts[(employee_idx, day_int - 1, shift_idx)] == 0)
 
             # whishes
-            if "wish_days" in employee:
-                for day_int in employee[
-                    "wish_days"
-                ]:  # day_int corresponds to real date, starts at 1
-                    for s in range(num_shifts):
-                        model.Add(
-                            shifts[(employee_idx, day_int - 1, s)] == 0
-                        )  # day_int to day_idx
-                    if day_int >= 2:  # prevent index of -1
-                        model.Add(
-                            shifts[(employee_idx, day_int - 2, 2)] == 0
-                        )  # no night shift before vacation
-            if "wish_shifts" in employee and len(employee["wish_shifts"][0]) > 0:
-                for day_int, shift_name in employee["wish_shifts"]:
-                    shift_idx = shift_names_to_index[shift_name]
-                    model.Add(shifts[(employee_idx, day_int - 1, shift_idx)] == 0)
+            if wishes_as_hard_constraint:
+                if "wish_days" in employee:
+                    for day_int in employee[
+                        "wish_days"
+                    ]:  # day_int corresponds to real date, starts at 1
+                        for s in range(num_shifts):
+                            model.Add(
+                                shifts[(employee_idx, day_int - 1, s)] == 0
+                            )  # day_int to day_idx
+                        if day_int >= 2:  # prevent index of -1
+                            model.Add(
+                                shifts[(employee_idx, day_int - 2, 2)] == 0
+                            )  # no night shift before vacation
+                if "wish_shifts" in employee and len(employee["wish_shifts"][0]) > 0:
+                    for day_int, shift_name in employee["wish_shifts"]:
+                        shift_idx = shift_names_to_index[shift_name]
+                        model.Add(shifts[(employee_idx, day_int - 1, shift_idx)] == 0)
 
     else:
         raise ValueError(
@@ -82,3 +85,5 @@ def add_free_shifts_and_vacation_days(
         )
 
     StateManager.state.constraints.append(NAME_OF_CONSTRAINT)
+    if wishes_as_hard_constraint:
+        StateManager.state.constraints.append("Wishes as Hard Constraint")
