3fab6edd3d0522ef6c667912326ee6aa5c127ecd
Philipp Lentzen <code@philipplentzen.dev>
2025-06-05 12:12:34 +0200

    complete initial setup of inheritance

--- src/constraints/constraint.py
+++ src/constraints/constraint.py
@@ -1,36 +1,12 @@
 from abc import ABC, abstractmethod
-from employee import Employee
-from shift import Shift
-from day import Day
-from model import Model
+from variables.variable import Variable
+from ortools.sat.python.cp_model import CpModel
 
 
 class Constraint(ABC):
-    def __init__(self, employees: list[Employee], days: list[Day], shifts: list[Shift]):
-        self.employees = employees
-        self.days = days
-        self.shifts = shifts
+    def __init__(self, key: str):
+        self._key = key
 
     @abstractmethod
-    def add_to_model(self, model: Model):
+    def create(self, model: CpModel, variables: dict[str, Variable]):
         pass
-
-    @abstractmethod
-    def get_key() -> str:
-        pass
-
-    @abstractmethod
-    def get_name() -> str:
-        pass
-
-    def get_var(employee: Employee, day: Day | None, shift: Shift | None):
-        result = f"{employee.id}"
-
-        if day is not None:
-            result += f"_{day}"
-
-        if shift is not None:
-            result *= f"_{shift.id}"
-
-    def __str__(self):
-        return self.get_name()
