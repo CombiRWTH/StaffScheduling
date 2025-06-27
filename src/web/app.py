fbf71ebf24c14683dd7bef559cff9f7b3a085d7c
Philipp Lentzen <code@philipplentzen.dev>
2025-06-27 13:43:40 +0200

    feat: add command for webpage
    #121

--- src/web/app.py
+++ src/web/app.py
@@ -1,25 +1,23 @@
 from flask import Flask, render_template, request
-from loader import FSLoader, Loader
-from datetime import date
+from loader import Loader
 from employee import Employee
-from day import Day
 from shift import Shift
+from re import match
+from datetime import datetime
 
 
 class App:
     _app: Flask
     _loader: Loader
     _employees: list[Employee]
-    _days: list[Day]
     _shifts: list[Shift]
 
-    def __init__(self, case_id: int, start_date: date):
+    def __init__(self, loader: Loader):
         self._app = Flask(__name__)
         self._app.add_url_rule("/", "index", self.index)
 
-        self._loader = FSLoader(case_id)
+        self._loader = loader
         self._employees = self._loader.get_employees()
-        self._days = self._loader.get_days(start_date)
         self._shifts = self._loader.get_shifts()
 
     def index(self):
@@ -29,6 +27,14 @@ class App:
         )
 
         solution = self._loader.get_solution(selected_solution_file_name)
+        start_date = min(
+            datetime.strptime(
+                match(r"\(\d+, '([\d-]+)', \d+\)", key).group(1), "%Y-%m-%d"
+            ).date()
+            for key in solution.variables.keys()
+            if match(r"\(\d+, '([\d-]+)', \d+\)", key)
+        )
+        days = self._loader.get_days(start_date)
 
         return render_template(
             "index.html",
@@ -36,9 +42,9 @@ class App:
             selected_solution_file_name=selected_solution_file_name,
             variables=solution.variables,
             employees=self._employees,
-            days=self._days,
+            days=days,
             shifts=self._shifts,
         )
 
-    def run(self):
-        self._app.run(debug=True, port=5020)
+    def run(self, debug: bool = False):
+        self._app.run(debug=debug, port=5020)
