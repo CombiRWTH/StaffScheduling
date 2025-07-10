ac7e4ff8d8a0e4174a78a402b392d6b4411832f4
iramisu <kiran.mishra@rwth-aachen.de>
2025-07-10 19:55:16 +0530

    Update analyze_solution.py

--- src/web/analyze_solution.py
+++ src/web/analyze_solution.py
@@ -75,6 +75,17 @@ def calculate_not_free_after_night_shift(schedule: Dict[datetime.date, int]) -> 
                 violations += 1
     return violations
 
+def calculate_granted_shift_wishes(
+    emp: Employee, schedule: Dict[datetime.date, int], shifts: List[Shift]
+) -> int:
+    count = 0
+    for wish_day, wish_shift_abbr in emp.get_wish_shifts:
+        for day, assigned_shift_id in schedule.items():
+            if day.day == wish_day:
+                if shifts[assigned_shift_id].abbreviation == wish_shift_abbr:
+                    count += 1
+    return count
+
 
 def analyze_solution(
     variables: Dict[str, int], employees: List[Employee], shifts: List[Shift]
@@ -90,6 +101,7 @@ def analyze_solution(
     total_overtime_hours = 0.0
     no_free_days_around_weekend = 0
     not_free_after_night_shift = 0
+    granted_wish_shifts = 0
 
     for key, value in variables.items():
         if value != 1:
@@ -120,6 +132,8 @@ def analyze_solution(
         no_free_days_around_weekend += calculate_no_free_days_around_weekend(schedule)
         not_free_after_night_shift += calculate_not_free_after_night_shift(schedule)
 
+        granted_wish_shifts += calculate_granted_shift_wishes(emp, schedule, shifts)
+
     return {
         "forward_rotation_violations": forward_rotation_violations,
         "consecutive_working_days_gt_5": consecutive_working_days_gt_5,
@@ -128,4 +142,5 @@ def analyze_solution(
         "total_overtime_hours": round(total_overtime_hours, 2),
         "no_free_days_around_weekend": no_free_days_around_weekend,
         "not_free_after_night_shift": not_free_after_night_shift,
+        "granted_wish_shifts": granted_wish_shifts,
     }
