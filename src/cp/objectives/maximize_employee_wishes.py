ea655d4f826e42525ca55241c7cf8ba421659d6e
iramisu <kiran.mishra@rwth-aachen.de>
2025-07-10 19:30:15 +0530

    Create maximize_employee_wishes.py

---
+++ src/cp/objectives/maximize_employee_wishes.py
@@ -0,0 +1,37 @@
+from . import Objective
+from ..variables import EmployeeDayShiftVariable, EmployeeDayVariable
+from ortools.sat.python.cp_model import CpModel, IntVar
+
+
+class MaximizeEmployeeWishesObjective(Objective):
+    KEY = "maximize-employee-wishes"
+
+    def create(self, model: CpModel, variables: dict[str, IntVar]):
+        rewards: list[IntVar] = []
+
+        for employee in self._employees:
+            # Wish to be OFF on specific days
+            for wish_day in employee.get_wish_days:
+                for day in self._days:
+                    if day.day == wish_day:
+                        var = variables[EmployeeDayVariable.get_key(employee, day)]
+                        wish_reward = model.NewBoolVar(f"wish_day_off_{employee.get_key()}_{day}")
+                        model.Add(var == 0).OnlyEnforceIf(wish_reward)
+                        rewards.append(wish_reward)
+
+            # Wish to work specific shifts
+            for wish_day, abbr in employee.get_wish_shifts:
+                for day in self._days:
+                    if day.day == wish_day:
+                        shift = next((s for s in self._shifts if s.abbreviation == abbr), None)
+                        if shift is None:
+                            continue
+                        key = EmployeeDayShiftVariable.get_key(employee, day, shift)
+                        var = variables[key]
+                        shift_reward = model.NewBoolVar(
+                            f"wish_shift_{employee.get_key()}_{day}_{abbr}"
+                        )
+                        model.Add(var == 1).OnlyEnforceIf(shift_reward)
+                        rewards.append(shift_reward)
+
+        return sum(rewards) * self.weight
