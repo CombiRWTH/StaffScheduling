04d61deea7d18f81614099eab7d44a29734e0952
Philipp Lentzen <code@philipplentzen.dev>
2025-07-11 03:49:10 +0200

    fix: typo in filename

--- src/cp/constraints/__init__.py
+++ src/cp/constraints/__init__.py
@@ -13,4 +13,4 @@ from .target_working_time import (
 from .vaction_days_and_shifts import (
     VacationDaysAndShiftsConstraint as VacationDaysAndShiftsConstraint,
 )
-from .planed_shifts import PlannedShiftsConstraint as PlannedShiftsConstraint
+from .planned_shifts import PlannedShiftsConstraint as PlannedShiftsConstraint
